de122d99812e6d5a8e982bae5ebb9d7698ae3d0e
sinaetown <ksw08215@gmail.com>
2023-11-15 10:30:03 +0900

    if-else using var

--- 3.if_statements_practice.sh
+++ 3.if_statements_practice.sh
@@ -1,5 +1,15 @@
-if [ -f "first_file.txt" ]; then
-    echo "file exists"
+# if [ -f "first_file.txt" ]; then
+#     echo "first_file.txt exists"
+# else 
+#     echo "first_file.txt does not exist"
+# fi
+mkdir mydir
+cd mydir
+touch first_txt second_txt
+
+file_name="first_file.txt"
+if [ -f $file_name ]; then
+    echo "$file_name exists"
 else 
-    echo "file does not exist"
+    echo "$file_name does not exist"
 fi
